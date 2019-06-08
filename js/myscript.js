8b80a38c7ac15832e5d2828b1d88e7079cb90c2f
Milan Starinac <milan_starinac@yahoo.com>
2019-06-08 18:00:36 +0200

    Final

--- js/myscript.js
+++ js/myscript.js
@@ -11,47 +11,61 @@ var i = 0;
 
 function incNumber() {
     if (i < 10000) {
-            i++;
-        } else if (i = 10) {
+        i++;
+        } else if (i = 1000000) {
             i = 0;
         }
         document.getElementById("display").innerHTML = i;
     }
 
-    var j = 0;
-
-function incNumber1() {
-        if (j < 10000) {
-            j++;
-        } else if (j = 10) {
-           j = 0;
-        }
-        document.getElementById("display1").innerHTML = j;
-    }
+    var k = 0;
 
     function incNumber1() {
-        if (i < 10000) {
-            i++;
-        } else if (i = 10) {
-            i = 0;
+        if (k < 10000) {
+            k++;
+        } else if (k = 1000000) {
+            k = 0;
         }
-        document.getElementById("display1").innerHTML = i;
+        document.getElementById("display1").innerHTML = k;
     }
 
-    function incNumber1() {
-        if (i < 10000) {
-            i++;
-        } else if (i = 10) {
-            i = 0;
+var m = 0;
+
+function incNumber2() {
+        if (m < 10000) {
+            m++;
+        } else if (m = 1000000) {
+            m = 0;
         }
-        document.getElementById("display1").innerHTML = i;
+        document.getElementById("display2").innerHTML = m;
     }
 
-    function incNumber1() {
-        if (i < 10000) {
-            i++;
-        } else if (i = 10) {
-            i = 0;
+var n = 0;
+
+    function incNumber3() {
+        if (n < 10000) {
+            n++;
+        } else if (n = 1000000) {
+            n = 0;
         }
-        document.getElementById("display1").innerHTML = i;
+        document.getElementById("display3").innerHTML = n;
+    }
+
+var z = 0;
+    function incNumber4() {
+        if (z < 10000) {
+            z++;
+        } else if (z = 1000000) {
+            z = 0;
+        }
+        document.getElementById("display4").innerHTML = z;
+    }
+var w = 0;
+    function incNumber5() {
+        if (w < 10000) {
+            w++;
+        } else if (w = 1000000) {
+            w = 0;
+        }
+        document.getElementById("display5").innerHTML = w;
     }
